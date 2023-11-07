95ae285b10a73284d967f333534af8e82c56e60a
Abdiel Ortega <nathanabdiel1@gmail.com>
2023-11-07 16:38:04 -0600

    Setup node to create redis client

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ import express from "express"
 import redis from "redis"
 
 const app = express()
-const client = redis.createClient()
+const client = redis.createClient({
+  host: "redis-server",
+  port: 6379,
+})
 client.set("visits", 0)
 
 app.get("/", (req, res) => {
