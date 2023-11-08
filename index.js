e4b27790003de6d53e4ce484ed4007a6c63eb341
Abdiel Ortega <nathanabdiel1@gmail.com>
2023-11-07 19:07:20 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ import express from "express"
 import redis from "redis"
 
 const app = express()
-const client = redis.createClient({
-  host: "redis-server",
-  port: 6379,
-})
-client.set("visits", 0)
-
-app.get("/", (req, res) => {
-  client.get("visits", (err, visits) => {
-    res.send(`Number of visits is ${visits}`)
-    client.set("visits", parseInt(visits) + 1)
+const client = await redis
+  .createClient({
+    url: "redis://default:default@redis-server:6379",
   })
+  .on("error", (err) => console.log("Redis Client Error", err))
+  .connect()
+
+await client.set("visits", 0)
+
+app.get("/", async (req, res) => {
+  const visits = await client.get("visits")
+  res.send(`Number of visits is ${visits}`)
+  await client.set("visits", parseInt(visits) + 1)
 })
 
 app.listen(8081, () => {
